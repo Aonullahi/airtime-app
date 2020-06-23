7d4409a3bbc4e41f2eafc090928d60bc50e35866
Aonullahi <adebayoaonullahi@yahoo.com>
2020-06-23 14:26:44 +0100

    adjusted root

--- airtime_api/settings.py
+++ airtime_api/settings.py
@@ -136,14 +136,16 @@ USE_TZ = True
 
 #MEDIA_URL = '/static/'
 
+
+
+
 #deploymentpip install whitenoise
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/'
-#STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'assets')
+STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
